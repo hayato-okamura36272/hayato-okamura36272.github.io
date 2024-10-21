5909d7bb17c3f55beb178fbb75a28fbf4314f3b7
HayatoOkamura <keepsmile.lucky7@gmail.com>
2024-10-21 14:22:39 +0900

    fix: 修正対応

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContentLoaded",()=>{console.log("test");const r=document.querySelectorAll("[data-observe]"),o={root:null,rootMargin:"0px",threshold:.1},n=(e,a)=>{e.forEach(t=>{t.isIntersecting&&(t.target.classList.add("visible"),a.unobserve(t.target))})},s=new IntersectionObserver(n,o);r.forEach(e=>{s.observe(e)})});
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("test");
+  const elements = document.querySelectorAll("[data-observe]");
+  const observerOptions = {
+    root: null,
+    // ビューポートをルートに設定
+    rootMargin: "0px",
+    threshold: 0.1
+    // 10%見えたら発火
+  };
+  const observerCallback = (entries, observer2) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer2.unobserve(entry.target);
+      }
+    });
+  };
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
