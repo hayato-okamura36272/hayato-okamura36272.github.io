ff230c570fe52e906a6a48f16748d0c98cf3b3f2
hayato-okamura36272 <hayato.okamura@telepathy.jp>
2025-01-05 21:51:51 +0900

    fix: デプロイ

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,1 +1,82 @@
-document.addEventListener("DOMContentLoaded",()=>{console.log("test");const n=()=>{const t=document.querySelectorAll("[data-observe]"),r={root:null,rootMargin:"0px",threshold:.1},s=(e,l)=>{e.forEach(c=>{c.isIntersecting&&(c.target.classList.add("visible"),l.unobserve(c.target))})},o=new IntersectionObserver(s,r);t.forEach(e=>{o.observe(e)})},d=()=>{const t=document.querySelectorAll(".price_section"),r=document.querySelector(".price_container__decoration"),s=document.querySelectorAll(".slide-image");s.forEach(e=>{e.style.opacity="0",e.style.visibility="hidden"});const o=new IntersectionObserver(e=>{e.forEach(l=>{if(l.isIntersecting){const c=l.target.getAttribute("data-image");r.classList.add("slide-in"),s.forEach(i=>{i.getAttribute("src")===c?(i.style.opacity="1",i.style.visibility="visible"):(i.style.opacity="0",i.style.visibility="hidden")})}})},{threshold:.5});t.forEach(e=>o.observe(e))};(()=>{const t=document.querySelector(".reserve_modal"),r=document.querySelectorAll("#reserve-btn");console.log(r);const s=document.querySelector(".reserve_modal");r.forEach(o=>{o.addEventListener("click",()=>{t.classList.remove("hidden"),t.classList.add("show")})}),s.addEventListener("click",o=>{o.target===s&&(t.classList.remove("show"),t.classList.add("hidden"))})})(),n(),d()});
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("test");
+  const scrollAnimation = () => {
+    const elements = document.querySelectorAll("[data-observe]");
+    const observerOptions = {
+      root: null,
+      // ビューポートをルートに設定
+      rootMargin: "0px",
+      threshold: 0.1
+      // 10%見えたら発火
+    };
+    const observerCallback = (entries, observer2) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer2.unobserve(entry.target);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+  };
+  const animationPrice = () => {
+    const sections = document.querySelectorAll(".price_section");
+    const imageContainer = document.querySelector(
+      ".price_container__decoration"
+    );
+    const images = document.querySelectorAll(".slide-image");
+    images.forEach((image) => {
+      image.style.opacity = "0";
+      image.style.visibility = "hidden";
+    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const imageUrl = entry.target.getAttribute("data-image");
+            imageContainer.classList.add("slide-in");
+            images.forEach((image) => {
+              const imageSrc = image.getAttribute("src");
+              if (imageSrc === imageUrl) {
+                image.style.opacity = "1";
+                image.style.visibility = "visible";
+              } else {
+                image.style.opacity = "0";
+                image.style.visibility = "hidden";
+              }
+            });
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+    sections.forEach((section) => observer.observe(section));
+  };
+  const reserveModal = () => {
+    const modal = document.querySelector(".reserve_modal");
+    const btns = document.querySelectorAll("#reserve-btn");
+    console.log(btns);
+    const modalBackground = document.querySelector(".reserve_modal");
+    btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modal.classList.remove("hidden");
+        modal.classList.add("show");
+      });
+    });
+    modalBackground.addEventListener("click", (event) => {
+      if (event.target === modalBackground) {
+        modal.classList.remove("show");
+        modal.classList.add("hidden");
+      }
+    });
+  };
+  reserveModal();
+  scrollAnimation();
+  animationPrice();
+});
